0698e35402eab8f389e1395f8ccd6ef85961c228
Daniel Crenna <daniel.crenna@gmail.com>
2020-03-25 13:56:30 -0400

    add OccurrenceTests and CronTemplatesTests

--- test/ActiveScheduler.Tests/OccurrenceTests.cs
+++ test/ActiveScheduler.Tests/OccurrenceTests.cs
@@ -1,22 +1,29 @@
 using System;
 using ActiveScheduler.Models;
+using Xunit;
 
 namespace ActiveScheduler.Tests
 {
 	public class OccurrenceTests
 	{
-		public bool Occurrence_is_in_UTC()
-		{
-            var task = new BackgroundTask
-            {
-	            RunAt = DateTimeOffset.UtcNow, 
-	            Expression = CronTemplates.Daily(1, 3, 30)
-            };
-			var next = task.NextOccurrence;
+		private readonly IBackgroundTaskStore _store;
 
-			return next != null && 
-			       next.Value.Hour == 3 && 
-			       next.Value.Hour.Equals(next.Value.UtcDateTime.Hour);
+		public OccurrenceTests()
+		{
+			_store = new InMemoryBackgroundTaskStore(()=> DateTimeOffset.Now);
+		}
+
+		[Fact]
+		public void Occurrence_is_in_UTC()
+		{
+            var task = new BackgroundTask {
+	            RunAt = _store.GetTaskTimestamp(), 
+	            Expression = CronTemplates.Daily(1, 3, 30)};
+
+            var next = task.NextOccurrence;
+			Assert.NotNull(next);
+			Assert.True(next.Value.Hour == 3);
+			Assert.Equal(next.Value.Hour, next.Value.UtcDateTime.Hour);
 		}
 	}
 }
